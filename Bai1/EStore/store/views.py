1303950424e775d0e719a20b7acc8508c5c201f5
zzhappyzebrazz <b8.bbl.lmh@gmail.com>
2022-12-01 22:16:43 +0700

    updating Bai1

--- Bai1/EStore/store/views.py
+++ Bai1/EStore/store/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from store.models import *
 
+#all products
+all_products = Product.objects.all()
+sub_category_1_list = [1, 2, 3, 4, 5]
+sub_category_2_list = [6, 7, 8, 9, 10]
 # Create your views here.
 def index(request):
-    return render(request, 'store/index.html')
+    #Slider
+    sliders = Slider.objects.all()
+    print(sliders)
+
+    #Brands
+    brands = Brand.objects.all()
+    print(brands)
+
+
+    #Thiết bị gia đình
+    sub_category_1 = [product for product in all_products if product.subcategory_id in sub_category_1_list]
+    print(f'Số lượng sản phẩm trong Thiết bị gia đình {len(sub_category_1)}')
+
+    #Đồ dùng nhà bếp
+    sub_category_2 = [product for product in all_products if product.subcategory_id in sub_category_2_list]
+    print(f'Số lượng sản phẩm trong Đồ dùng nhà bếp {len(sub_category_2)}')
+
+    return render(request, 'store/index.html',{
+        'sub_category_1' : sub_category_1,
+        'sub_category_2' : sub_category_2,
+        'sliders' : sliders,
+        'brands' : brands,
+    })
 
 def cart(request):
     return render(request, 'store/cart.html')
